2b3d7c83271408e3498129b973ed87fd30cf5e68
Adam Savitzky <asavitzky@forio.com>
2012-10-09 17:33:04 -0700

    Added gasoline tutorial

---
+++ intermediate/4-statistics/main.jl
@@ -0,0 +1,15 @@
+load("ols.jl")
+
+data = csvread("gasoline.csv")
+
+# Get the columns that correspond with our X values 
+# and create a new matrix to hold them
+# syntax: data[row, column]
+# to get a whole column do data[:, column]
+# to get a whole row do data[row, :]
+x = [data[:,2] data[:,3] data[:,4]]
+y = data[:,6]
+
+# Create a new OLS object
+reg = ols(y, x, "Octane Rating", ["Error", "Component 1", "Component 2", "Component 3"])
+summary(reg)
