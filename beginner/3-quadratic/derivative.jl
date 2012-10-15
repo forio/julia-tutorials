919e48c6009cb07c49dcece8f705048e7e40ff44
Adam Savitzky <asavitzky@forio.com>
2012-10-15 14:27:49 -0700

    Change epsilon back to eps(Float64)

--- beginner/3-quadratic/derivative.jl
+++ beginner/3-quadratic/derivative.jl
@@ -1,6 +1,6 @@
 function derivative(fn)
     return x ->begin
-        h = x == 0 ? sqrt(eps(x)) : sqrt(eps(x)) * x
+        h = x == 0 ? sqrt(eps(Float64)) : sqrt(eps(Float64)) * x
        
 		 xph = x + h
         dx = xph - x
